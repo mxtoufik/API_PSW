3ba0fa31061801536dc5a9c019e0090239beb1bd
CrazyJMB <crazyjmbemail@gmail.com>
2023-03-05 19:45:21 +0100

    Base API mejorada - Arreglo errores

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,14 +1,9 @@
-// import { getConnection as db } from "../database.js";
-
+const db = require("../database");
 const ping = (req, res) => res.send("pong");
 
 const getUsers = async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT * FROM usuarios");
-    res.json(rows);
-  } catch (error) {
-    return res.status(500).json({ message: "Something goes wrong" });
-  }
+  const [rows] = await db.pool.query("SELECT * FROM participante");
+  res.json(rows);
 };
 
 module.exports = {
--- database.js
+++ database.js
@@ -1,11 +1,12 @@
-const { createPool } = require("mysql2");
+const { createPool } = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: 3000,
+  port: process.env.PORT,
 });
 
 module.exports = {
